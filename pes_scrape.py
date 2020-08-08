c338f5b94cef8d2125c76121e84b7fa429350a8a
Narasimha <jwalapuramnarasimha@gmail.com>
2020-08-08 22:16:48 +0530

    PES scrape change

--- pes_scrape.py
+++ pes_scrape.py
@@ -1,1 +1,12 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/narasimhajwalapuram/Downloads/chromedriver')
+
+driver.get('https://pesuacademy.com/Academy/')
+
+driver.find_element_by_id('knowClsSection').click()
+
+
+
 
